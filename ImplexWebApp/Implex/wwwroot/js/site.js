2a3d8b4f9793b3968c32f5f6639640dd27a1eaab
anastasia.baklayeva <nasty199961@gmail.com>
2020-05-25 03:10:14 +0300

    Add styles and delete useless

--- ImplexWebApp/Implex/wwwroot/js/site.js
+++ ImplexWebApp/Implex/wwwroot/js/site.js
@@ -48,3 +48,23 @@ function generateMatrixMultiplicationInput(matrixAInputId, matrixBInputId, nSize
     generateMatrixInput(matrixAInputId, nSize, mSize);
     generateMatrixInput(matrixBInputId, mSize, pSize);
 }
+
+let animateButton = function (e) {
+
+    e.preventDefault;
+    //reset animation
+    e.target.classList.remove('animate');
+
+    e.target.classList.add('animate');
+
+    e.target.classList.add('animate');
+    setTimeout(function () {
+        e.target.classList.remove('animate');
+    }, 6000);
+};
+
+const classname = document.getElementsByClassName("button");
+
+for (let i = 0; i < classname.length; i++) {
+    classname[i].addEventListener('click', animateButton, false);
+} 
